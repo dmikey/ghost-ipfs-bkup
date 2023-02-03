86e6d1cd0a802ca03e0143bcb3443cd34f2984b2
Derek Anderson <derek@txlabs.org>
2023-02-03 17:09:19 -0600

    this works better right now

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # prep ghost stuff from entry point script
-/usr/local/bin/docker-entrypoint.sh
+baseDir="$GHOST_INSTALL/content.orig"
+for src in "$baseDir"/*/ "$baseDir"/themes/*; do
+    src="${src%/}"
+    target="$GHOST_CONTENT/${src#$baseDir/}"
+    mkdir -p "$(dirname "$target")"
+    if [ ! -e "$target" ]; then
+        tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
+    fi
+done
 
 # run restore
 /usr/local/bin/restore.sh
